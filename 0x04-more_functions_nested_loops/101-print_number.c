ec410cbd28183ccd56c89aac44a3421f16513a3d
GodwinAjor <Godwinajor>
2022-09-15 14:28:36 -0700

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,23 +1,44 @@
 #include "main.h"
 /**
- *print_number - print integer.
- *@n: integer.
- *
- *Return: void.
+ * print_number - prints an integer
+ * @na: integer to print
+ * Return: nothing
  */
-void print_number(int n)
+void print_number(int na)
 {
-	unsigned int x;
+	unsigned int pc, i, z, nmodul, n;
 
-	x = n;
-	if (n < 0)
+	if (na == 0)
+		_putchar('0');
+	/* if n is negative we make it positive*/
+	if (na < 0)
 	{
-		_putchar(45);
-		x = -n;
+		n = -na;
+		_putchar('-');
 	}
-	if (x / 10)
-		print_number(x / 10);
-	_putchar((x % 10) + '0');
+	else
+		n = na;
+	if (n < 10 && n > 0)
+		_putchar(n + '0');
+	while (n >= 10)
+	{
+		pc = n;
+		z = 0;
+		nmodul = 0;
+		for (i = 0; pc >= 10; i++)
+		{
+			if (pc / 10 > 0 && pc % 10 == 0)
+				z++;
+			else
+				z = 0;
+			pc /= 10;
+			nmodul =  (nmodul) ? nmodul * 10 : 10;
+		}
+		_putchar(pc + '0');
+		while (z-- > 0)
+			_putchar('0');
+		n %= nmodul;
+		if (n > 0 && n < 10)
+			_putchar(n + '0');
+	}
 }
-
-
