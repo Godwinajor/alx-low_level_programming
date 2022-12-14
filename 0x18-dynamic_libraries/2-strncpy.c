ce2243167eaf84f96d2635d511c2e8c46ec6791c
GodwinAjor <Godwinajor>
2022-12-13 17:28:17 -0800

    Dynamic libraries

--- 0x18-dynamic_libraries/2-strncpy.c
+++ 0x18-dynamic_libraries/2-strncpy.c
@@ -2,29 +2,19 @@
 
 /**
  * _strncpy - copies a string
- * @dest: destination string
- * @src: source string
+ * @dest: point to destinatin
+ * @src: string to copy
  * @n: number of bytes to copy
- *
- * Return: pointer to the resulting string
+ * Return: @dest
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	i = 0;
-
-	while (src[i] != '\0' && i < n)
-	{
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-		i++;
-	}
-
-	while (i < n)
-	{
+	for (; i < n; i++)
 		dest[i] = '\0';
-		i++;
-	}
-
 	return (dest);
 }
