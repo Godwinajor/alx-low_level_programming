15c8814a7ef8ed9d2b459eebd31fc9af5a049ea4
Godwin Ajor <111155796+Godwinajor@users.noreply.github.com>
2023-01-06 17:38:43 +0100

    Update 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,27 +1,33 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - retrieves a value associated with a key
- * @ht: is the hash table we want to look into
- * @key: the key we are looking for
- * Return: value associated with the element, or NULL
-**/
+ * hash_table_get - function that retrieves a value associated with a key
+ * @ht: Double pointer of type hash_table_t for hash table
+ * @key: Const char pointer for unique value to be searched for
+ * Return: The value associated with the element found, NULL if key couldn't be
+ * found
+ */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	hash_node_t *nodo;
+	hash_node_t *temp;
 	unsigned long int index;
 
-	if (key == NULL || ht == NULL)
+	if (ht == NULL || key == NULL)
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	nodo = ht->array[index];
 
-	for (; nodo != NULL; nodo = nodo->next)
+	if (ht->array[index] == NULL)
+		return (NULL);
+
+	temp = ht->array[index];
+
+	while (temp != NULL)
 	{
-		if (strcmp(nodo->key, key) == 0)
-			return (nodo->value);
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
+		temp = temp->next;
 	}
 
 	return (NULL);
