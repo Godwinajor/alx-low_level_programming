10328c013727129404ce736a1baaba39725b87aa
Godwin Ajor <111155796+Godwinajor@users.noreply.github.com>
2023-01-06 17:40:33 +0100

    Update 5-hasdh_table_print.c

--- 0x1A-hash_tables/5-hasdh_table_print.c
+++ 0x1A-hash_tables/5-hasdh_table_print.c
@@ -1,33 +1,46 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print - prints a hash table
- * @ht: is the hash table
-**/
+ * hash_table_print - function that prints a hash table.
+ * @ht: Double pointer of type hash_table_t for hash table
+ * Return: Void
+ */
 
 void hash_table_print(const hash_table_t *ht)
 {
-	hash_node_t *node;
-	unsigned long int cont = 0, i = 0;
+	hash_node_t *temp;
+	unsigned long int i, first_flag = 0;
 
-	if (ht != NULL)
+	if (ht == NULL)
+		return;
+
+	putchar('{');
+
+	for (i = 0; i < ht->size; i++)
 	{
-		printf("{");
-
-		for (cont = 0; cont < ht->size; cont++)
+		if (ht->array[i] != NULL)
 		{
-			node = ht->array[cont];
+			if (first_flag == 1)
+				printf(", ");
 
-			while (node != NULL)
+			temp = ht->array[i];
+
+			while (temp != NULL)
 			{
-				if (i != 0)
-					printf(", ");
-				i = 1;
-				printf("'%s': '%s'", node->key, node->value);
-				node = node->next;
+				if (temp->next == NULL)
+				{
+					printf("'%s': ", temp->key);
+					printf("'%s'", temp->value);
+					break;
+				}
+				printf("'%s': '%s', ", temp->key, temp->value);
+				temp = temp->next;
 			}
+			first_flag = 1;
 		}
 
-		printf("}\n");
+		if (ht->array[i] == NULL)
+			continue;
 	}
+	printf("}\n");
 }
