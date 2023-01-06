ab69337f024b2a02680f6bf86b148da5450191e6
Godwin Ajor <111155796+Godwinajor@users.noreply.github.com>
2023-01-06 17:30:33 +0100

    Update 2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,14 +1,20 @@
 #include "hash_tables.h"
 
+
 /**
- * key_index - gives you the index of a key
- * @key: is the key
- * @size: is the size of the array of the hash table
- * Return: the index at which the key/value pair
- * should be stored in the array of the hash table
-**/
+ * key_index - function that gives the index of a key.
+ * @key: Const unsigned char - unique value passed
+ * @size: Unsigned long int - size of array
+ * Return: Unsigned long int of index where the key is stored
+ */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	return (hash_djb2(key) % size);
+
+	unsigned long int index;
+
+	index = hash_djb2(key) % size;
+
+	return (index);
+
 }
